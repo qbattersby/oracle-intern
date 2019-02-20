665deb8fa97a1b4b339f2d4146f8a726108f5f41
Quinn Battersby <qbattersby@me.com>
2019-02-19 21:29:02 -0500

    Starting team page

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="grid-container">
-    <div class="grid-x text-center">
-      <div class="cell">
-        <h1>{{ msg }}</h1>
-        <a class="button secondary call-button" data-toggle="offCanvas"><i class="icon-puzzle"></i>JS Components</a>
-        <a class="button secondary call-button" href="https://github.com/vue-foundation/vue-foundation"><i class="icon-github-circled"></i>Source</a>
+  <div class="resources-page">
+    <div class="callout">
+      <div class="callout-content grid-container text-center">
+        <h1 class="callout-heading">Meet your Team</h1>
+        <div class="callout-copy">
+          <p>At Oracle we win as a team. Here you'll find your winning team,<br class="hide-for-small-only"> from mentors to fellow interns. Get ready to fly with the best. </p>
+        </div>
       </div>
     </div>
+
+    <div class="subnav">
+      <ul id="team-nav" class="menu align-center" data-smooth-scroll>
+        <li><a href="#general">General Resources</a></li>
+        <li><a href="#marketing">Marketing, Brand & Editorial Resources</a></li>
+        <li><a href="#transportation">Transportation</a></li>
+        <li><a href="#events">Events</a></li>
+      </ul>
+    </div>
+
+
+
+
+    <section class="team-footer">
+      <div class="section-callout text-center">
+        <h2>Meet Your Mentors</h2>
+        <p>Our mentors have a wealth of knowledge and experience to share and are committed to making this year’s intern experience the best ever.</p>
+      </div>
+    </section>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'team',
+  mounted() {
+    this.scroll = new Foundation.SmoothScroll($('#resources-nav'));
+  },
   data() {
     return {
-      msg: 'Meet the Team',
+      msg: '',
     };
   },
 };
@@ -24,18 +48,48 @@ export default {
 
 <style lang="scss" scoped>
 
-  .image {
-    margin-top: 0.75rem;
-    margin-bottom: 1.5rem;
+
+  .callout {
+    background-image: url('../assets/img/team-bg.svg');
+    background-size: cover;
+    background-position: center right;
+    position:relative;
+    @include breakpoint(large only) {
+      background-position: bottom left;
+    }
+    @include breakpoint(medium down) {
+      background-position: bottom center;
+      background-size: cover;
+    }
   }
 
-  .call-button {
-    border-radius: 20px;
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-    font-weight: 600;
-    text-transform: uppercase;
+  .team-footer {
+    background-image: url('../assets/img/team-footer-bg.svg');
+    background-size: cover;
+    background-repeat:no-repeat;
+    background-position: bottom right;
+    position:relative;
+    padding-bottom:480px;
+    background-color: #eeefef;
+    @include breakpoint(large only) {
+      background-position: bottom left;
+    }
+    @include breakpoint(medium down) {
+      background-position: bottom right;
+      background-size: cover;
+    }
   }
 
+  .callout-content {
+    margin-top:30vh;
+    opacity:0;
+    animation:fadeIn ease-in 1;
+    animation-fill-mode:forwards;
+    animation-duration:0.5s;
+    animation-delay: 0.5s;
+    @include breakpoint(small only) {
+      margin-top:23vh;
+    }
+  }
 
 </style>
